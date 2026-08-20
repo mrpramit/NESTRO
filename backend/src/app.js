d43dcf79efbe3f823ffcfebc4b6324792168b7ee
mrpramit <pramitsachan1234@gmail.com>
2026-08-20 15:23:52 +0530

    mongodb start

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,11 +25,8 @@ server.use("/api/order", orderRouter);
 
 
 connectDB().then(() => {
-    server.listen(process.env.PORT, "0.0.0.0", () => {
+    server.listen(process.env.PORT, () => {
         console.log(`Server is running on port ${process.env.PORT}`)
         console.log("backend started")
     })
-}).catch((error) => {
-    console.error("Database connection failed:", error);
-    process.exit(1);
 });
